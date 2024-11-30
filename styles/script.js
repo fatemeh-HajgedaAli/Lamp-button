9b174900b7f7f65379fc9df772e36fa2af701732
fatemeh-HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2024-11-30 19:35:49 +0330

    turn On/Off Lamp

---
+++ styles/script.js
@@ -0,0 +1,16 @@
+let  lampImage=document.querySelector('.lampImage')
+let clickBtn=document.querySelector('.clickBtn')
+let lampFlag=false
+
+function turnOnOff(){
+if(lampFlag){
+    clickBtn.innerHTML="turn on"
+    lampImage.setAttribute('src','pictures/offLamp.png')
+    lampFlag=false
+}else{
+    clickBtn.innerHTML="turn off"
+    lampImage.setAttribute('src','pictures/onLamp.png')
+    lampFlag=true
+}
+
+}
